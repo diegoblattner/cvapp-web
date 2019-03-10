5ba2085ebfdbd384217d56d2c42a8c28a5de89df
diegoblattner <15245871+diegoblattner@users.noreply.github.com>
2019-03-10 00:36:26 +0000

    feat(experience): adding styles (#6)

--- src/components/Role/component.jsx
+++ src/components/Role/component.jsx
@@ -1,9 +1,13 @@
 import { h } from 'preact';
+import styles from './styles.scss';
 
 const Role = ({ name, startDate, endDate = 'Now' }) => {
   return (
-    <li>
-      <strong>{name}</strong> ({startDate} - {endDate})
+    <li className={styles.role}>
+      <strong className={styles.role__name}>{name}</strong>
+      <span className={styles.role__period}>
+        {startDate} - {endDate}
+      </span>
     </li>
   );
 };
