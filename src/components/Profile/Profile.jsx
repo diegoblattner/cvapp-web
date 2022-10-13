f6120c1d97a3411fc7c83dd9399086b42a62ecf6
diegoblattner <15245871+diegoblattner@users.noreply.github.com>
2022-10-13 16:52:25 +0100

    links in header (#12)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,20 @@
 import { h } from 'preact';
+import { Links } from '../Links/Links';
 import * as styles from './styles.module.scss';
 
-const Profile = profileProps => {
+const Profile = (profileProps) => {
   const { avatar, name, position } = profileProps;
 
   return (
-    <header className={styles.profile}>
-      <img className={styles.profile__avatar} src={avatar} alt={name} />
-      <h1 className={styles.profile__name}>{name}</h1>
-      <h2 className={styles.profile__position}>{position}</h2>
+    <header className={styles.header}>
+      <div className={styles.profile}>
+        <img className={styles.profile__avatar} src={avatar} alt={name} />
+        <h1 className={styles.profile__name}>{name}</h1>
+        <h2 className={styles.profile__position}>{position}</h2>
+      </div>
+      <div className={styles.links}>
+        <Links links={profileProps} />
+      </div>
     </header>
   );
 };
