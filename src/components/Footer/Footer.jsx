c09b87595ef074da811b35de0c3adf99a244c01a
diegoblattner <15245871+diegoblattner@users.noreply.github.com>
2022-11-11 12:27:42 +0000

    lighthouse score fixes (#20)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => (
         This website is a personal side project with educational and portifolio purposes only. It may not reflect the most up to date data at all times. No information contained in this page can be used for any legal purposes.
       </li>
       <li className={styles.li}>
-        The source code for this webpage can be found <Link href="https://github.com/diegoblattner/cvapp-web" text="here" />
+        The source code for this webpage can be found on <Link href="https://github.com/diegoblattner/cvapp-web" text="GitHub" />
       </li>
       <li className={styles.li}>
         The icons used in this webpage come from <Link href="https://icomoon.io/" text="IcoMoon" />
